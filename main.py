0f41eac31a7019a42eb2f31d444715a2453174f6
ZiyuLiu <49537782+MoonmanBye@users.noreply.github.com>
2023-02-15 09:08:23 -0700

    Update main.py

--- main.py
+++ main.py
@@ -45,8 +45,13 @@ if not os.path.exists(args.outdir):
 if args.gpu:
     os.environ['CUDA_VISIBLE_DEVICES'] = args.gpu
 
+#set random seed
 np.random.seed(args.seed)
 torch.manual_seed(args.seed)
+torch.cuda.manual_seed_all(args.seed)
+random.seed(args.seed)
+torch.backends.cudnn.deterministic = True
+
 gpu_list = [int(i) for i in args.gpu.strip().split(",")] if args.gpu is not "0" else [0]
 if args.gpu == "-1":
     device = torch.device('cpu')
