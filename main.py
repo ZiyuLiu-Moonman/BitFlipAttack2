af6a131478c42bb4dbaca8a322e9632f3e83e25a
ZiyuLiu <49537782+MoonmanBye@users.noreply.github.com>
2023-02-17 16:04:42 -0700

    Update main.py

--- main.py
+++ main.py
@@ -101,39 +101,14 @@ def train(loader, model, criterion, optimizer, epoch, C):
         loss.backward(retain_graph=True)
         
         
-        #add noise
-        ori_grad =model.module.linear.weight.grad.clone()
-        #var_list.append(torch.var(ori_grad, unbiased=False))
-        ori_grad = torch.autograd.Variable(ori_grad, requires_grad=True)         
-        #rand_grad = 10*torch.rand_like(ori_grad).cuda()
-        loss_grad = args.coefficiency * criterion_grad(ori_grad,rand_fix)
-        loss_grad.backward()
         
-        
-        '''
-        ori_grad = model.module.linear.weight.grad.clone()
-        ori_grad = torch.autograd.Variable(ori_grad, requires_grad=True)
-        loss_grad = criterion_grad(ori_grad,rand_grad_fix.detach())
-        loss_grad.backward()
-        '''
-        
-        '''
-        #add noise version2
-        model.module.linear.weight.grad = model.module.linear.weight.grad + torch.randn_like(model.module.linear.weight.grad)
-        '''
-        
-        '''
-        linear_grad_max =  model.module.linear.weight.grad.abs().max().item()
-        '''
-        
-        '''
         #Use reciprocal
         ori_grad =model.module.linear.weight.grad.clone()
         ori_grad = torch.autograd.Variable(ori_grad, requires_grad=True)         
         reciprocal_grad = 100*torch.reciprocal(ori_grad).cuda()
-        loss_grad = criterion_grad(ori_grad,reciprocal_grad)
+        loss_grad = args.coefficiency * criterion_grad(ori_grad,reciprocal_grad)
         loss_grad.backward()
-        '''
+        
         
         optimizer.step()
 
