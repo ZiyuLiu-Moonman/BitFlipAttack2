5e7a457a34e96f100186267882d33742dbb83dd1
ZiyuLiu <49537782+MoonmanBye@users.noreply.github.com>
2023-02-17 16:27:39 -0700

    Update reciprocal.sh

--- reciprocal.sh
+++ reciprocal.sh
@@ -8,8 +8,8 @@ model="resnet20_quan"
 classes=10
 
 # Train ResNet-20 models (8-bit) with output code matching on CIFAR-10
-python -u main.py --data_dir $dir --dataset $data -c $classes --arch $model --bits 8 -e 2 --outdir "results/cifar10/coe10/" -coe 10
-python -u main.py --data_dir $dir --dataset $data -c $classes --arch $model --bits 8 -e 2 --outdir "results/cifar10/coe100/" -coe 100
+python -u main.py --data_dir $dir --dataset $data -c $classes --arch $model --bits 8 --outdir "results/cifar10/coe10/" -coe 10
+python -u main.py --data_dir $dir --dataset $data -c $classes --arch $model --bits 8 --outdir "results/cifar10/coe100/" -coe 100
 
 # Evaluate Stealthy T-BFA attacks on OCM defended models
 python -u attack_tbfa.py --data_dir $dir --dataset $data -c $classes --arch $model --bits 8 --outdir "results/cifar10/coe10/"
