01202e9269ff24f46eed678d5c654388b8ca1579
ZiyuLiu <49537782+MoonmanBye@users.noreply.github.com>
2023-02-16 17:03:59 -0700

    Update main.py

--- main.py
+++ main.py
@@ -112,7 +112,7 @@ def train(loader, model, criterion, optimizer, epoch, C):
         #var_list.append(torch.var(ori_grad, unbiased=False))
         ori_grad = torch.autograd.Variable(ori_grad, requires_grad=True)         
         #rand_grad = 10*torch.rand_like(ori_grad).cuda()
-        loss_grad = args.cov * criterion_grad(ori_grad,rand_fix)
+        loss_grad = args.coefficiency * criterion_grad(ori_grad,rand_fix)
         loss_grad.backward()
         
         
